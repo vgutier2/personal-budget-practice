402476c53488b7345fc2bf8b319092bc3bee7c3b
Velina Gutierrez <velinagutierrez@Velinas-MBP.lan>
2020-12-01 19:01:54 -0500

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { AccountComponent } from './account/account.component';
+import { P404Component } from './p404/p404.component';
 
-const routes: Routes = [];
+const routes: Routes =
+[
+  {
+    path: '',
+    component: HomepageComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'account',
+    component: AccountComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: P404Component,
+    pathMatch: 'full'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { P404Component } from './p404/p404.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AccountComponent } from './account/account.component';
+import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    LoginComponent,
+    P404Component,
+    NavigationComponent,
+    AccountComponent,
+    HeroComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
